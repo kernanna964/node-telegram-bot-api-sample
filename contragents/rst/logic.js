9c1e64dbbc7f66b14a1ad1b3d3e802a4ad420e92
kernanna964 <92305199+kernanna964@users.noreply.github.com>
2022-03-24 17:16:00 +0300

    Update logic.js

--- contragents/rst/logic.js
+++ contragents/rst/logic.js
@@ -15,7 +15,7 @@ module.exports = (bot) => {
         options = {
           reply_markup: JSON.stringify({
             inline_keyboard: [
-              [{ text: 'Информациионный блок', callback_data: 'rst.info' }],
+              [{ text: 'Информационный блок', callback_data: 'rst.info' }],
               [{ text: 'Логотипы', callback_data: 'rst.logo' }],
               [{ text: 'Бланки', callback_data: 'rst.blanks' }],
               [{ text: 'Профайлы', callback_data: 'rst.profiles' }],
