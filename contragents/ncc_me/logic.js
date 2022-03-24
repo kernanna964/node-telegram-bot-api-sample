0af57d736e14d0bc78b4dd92019d37c538a9d56f
kernanna964 <92305199+kernanna964@users.noreply.github.com>
2022-03-24 18:57:01 +0300

    Add files via upload

---
+++ contragents/ncc_me/logic.js
@@ -0,0 +1,69 @@
+const data = require('./data.json')
+
+const abSort = (a, b) => a.localeCompare(b);
+
+module.exports = (bot) => {
+  // Handle callback queries
+  bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+    const action = callbackQuery.data;
+    const msg = callbackQuery.message;
+    let text = 'Sorry, in progress...';
+    let options
+    switch(action) {
+      case 'ncc':
+        text = 'Выберите материалы НКК';
+        options = {
+          reply_markup: JSON.stringify({
+            inline_keyboard: [
+              [{ text: 'Информациионный блок', callback_data: 'ncc.info' }],
+              [{ text: 'Логотипы', callback_data: 'ncc.logo' }],
+              [{ text: 'Бланки', callback_data: 'ncc.blanks' }],
+              [{ text: 'Профайлы', callback_data: 'ncc.profiles' }],
+              [{ text: 'Презентации', callback_data: 'ncc.presentations' }],
+            ]
+          })
+        };
+        bot.sendMessage(msg.chat.id, text, options);
+        return
+
+      case 'ncc.info':
+        text = data.ncc.info.sort(abSort).join('\n\n');
+        bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
+        return
+      case 'ncc.logo':
+        text = data.ncc.logo.sort(abSort).join('\n\n');
+        bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
+        return
+      case 'ncc.blanks':
+        text = data.ncc.blanks.sort(abSort).join('\n\n');
+        bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
+        return
+      case 'ncc.profiles':
+        text = data.ncc.profiles.sort(abSort).join('\n\n');
+        bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
+        return
+		case 'ncc.presentations':
+        text = 'Выберите презентации НКК';
+        options = {
+          reply_markup: JSON.stringify({
+            inline_keyboard: [
+              [{ text: 'Общие', callback_data: 'ncc.presentations.general' }, { text: 'Отраслевые', callback_data: 'ncc.presentations.industry' }],
+            ]
+          })
+        };
+        bot.sendMessage(msg.chat.id, text, options);
+        return
+      case 'ncc.presentations.general':
+        text = data.ncc.presentations.general.sort(abSort).join('\n\n');
+        bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
+        return
+      case 'ncc.presentations.industry':
+        text = data.ncc.presentations.industry.sort(abSort).join('\n\n');
+        bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
+        return
+
+      default:
+        return
+    }
+  });
+}
