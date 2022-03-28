db278bb58e262619a1f2db849154cdff26fb9dc8
kernanna964 <92305199+kernanna964@users.noreply.github.com>
2022-03-28 12:35:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -56,14 +56,13 @@ bot.onText(/(\/menu|Menu|\/baza|Baza|gcs)/, function(msg) {
   const options = {
     reply_markup: JSON.stringify({
       inline_keyboard: [
-        [{ text: 'ГКС', callback_data: 'gcs' }],
+        [{ text: 'ГКС', callback_data: 'gcs' }, { text: 'NCC ME', callback_data: 'ncc_me' }],
         [{ text: 'Систематика', callback_data: 'systematica' }, { text: 'STEP LOGIC', callback_data: 'step_logic' }],
         [{ text: 'HeadPoint', callback_data: 'haed_point' }, { text: 'Landata', callback_data: 'landata' }],
         [{ text: 'Энсис Технологии', callback_data: 'ensys' }, { text: 'Систематика Консалтинг', callback_data: 'systematica_consulting' }],
         [{ text: 'Доверенная среда', callback_data: 'doverennay_sreda' }, { text: 'Tops BI', callback_data: 'tops_bi' }],
         [{ text: 'Lanmax', callback_data: 'lanmax' }, { text: 'СистематикаБел', callback_data: 'systematica_bel' }],
         [{ text: 'НКК', callback_data: 'ncc' }, { text: 'AQUARIUS', callback_data: 'aquarius' }],
-        [{ text: 'NCC ME', callback_data: 'ncc_me' }],
         [{ text: 'РСТ-Инвент', callback_data: 'rst' }, { text: 'Национальная платформа', callback_data: 'national_platform' }],
       ],
     })
