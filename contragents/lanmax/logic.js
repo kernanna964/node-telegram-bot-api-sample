81f7b88de16754dc43da809334355f4d45d25697
kernanna964 <92305199+kernanna964@users.noreply.github.com>
2023-02-14 13:04:03 +0300

    Update logic.js

--- contragents/lanmax/logic.js
+++ contragents/lanmax/logic.js
@@ -17,6 +17,7 @@ module.exports = (bot) => {
             inline_keyboard: [
               [{ text: 'Информационный блок', callback_data: 'lanmax.info' }],
               [{ text: 'Логотипы', callback_data: 'lanmax.logo' }],
+              [{ text: 'Бланки', callback_data: 'lanmax.blanks' }],
               [{ text: 'Профайлы', callback_data: 'lanmax.presentations' }],
             ]
           })
@@ -30,6 +31,10 @@ module.exports = (bot) => {
         return
       case 'lanmax.logo':
         text = data.lanmax.logo.sort(abSort).join('\n\n');
+        bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
+        return
+      case 'lanmax.blanks':
+        text = data.lanmax.blanks.sort(abSort).join('\n\n');
         bot.sendMessage(msg.chat.id, text, { parse_mode: "Markdown" });
         return
       case 'lanmax.presentations':
